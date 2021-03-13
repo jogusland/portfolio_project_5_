ff3f995179758339aa9643334fa89371ec28ebd8
Jogu <jogupersonnel@gmail.com>
2021-03-13 00:40:44 -0800

    Draft 1 - works great; added a search filter

--- js/app.js
+++ js/app.js
@@ -1,16 +1,2 @@
 baguetteBox.run('.wrapper');
-
-function search_photo() {
-    let input = document.getElementById('searchbar').value
-    input = input.toLowerCase();
-    let x = document.getElementsByClassName('photo');
-
-    for (i = 0; i < x.length; i++) {
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display = "none";
-        }
-        else {
-            x[i].style.display = "list-item";
-        }
-    }
-}
+const search = new Filter('search', 'data-caption');
